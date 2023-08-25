edf202b9591480ee19862dc135ddfd13534e5553
rani10245kumari <131008023+rani10245kumari@users.noreply.github.com>
2023-08-25 11:03:57 +0530

    Update Functioncompo.js

--- src/Handson1React/Functioncompo.js
+++ src/Handson1React/Functioncompo.js
@@ -4,7 +4,7 @@ function Functionnew() {
         <div className="one">
             <h1>This is created using functional component</h1>
             <h3>This is done using external css</h3>
-            <p style={{ color: "blue", fontSize: "38px", textAlign: "center" }}>This is done using inline css</p>
+            <p style={{ color: "blue", fontSize: "28px", textAlign: "center", fontWeight: "600" }}>This is done using inline css</p>
         </div>
     )
 }
