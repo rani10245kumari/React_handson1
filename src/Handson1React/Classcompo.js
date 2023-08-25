029b48c5897cf132d5ea10543cbc46087c037d96
rani10245kumari <131008023+rani10245kumari@users.noreply.github.com>
2023-08-25 11:04:33 +0530

    Update Classcompo.js

--- src/Handson1React/Classcompo.js
+++ src/Handson1React/Classcompo.js
@@ -1,4 +1,4 @@
-
+import "./style.css"
 import { Component } from 'react'
 class Classfunction extends Component {
     render() {
@@ -6,7 +6,7 @@ class Classfunction extends Component {
             <div className="two">
                 <h1>This is created using class component</h1>
                 <h3>This is done using external  css</h3>
-                <p style={{ color: "blue", fontSize: "38px", textAlign: "center" }}>This is done using inline css</p>
+                <p style={{ color: "blue", fontSize: "28px", textAlign: "center", fontWeight: "600" }}>This is done using inline css</p>
             </div>
         )
     }
